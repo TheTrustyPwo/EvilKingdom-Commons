bb92bd6396e6255b4ebf8c21f1b86079846202d8
kodirati <me@kodirati.com>
2022-05-01 04:05:47 -0500

    Light LuckPermsUtilities changes to make it easier for players that we KNOW are online.
    Signed-off-by: kodirati <me@kodirati.com>
    
    Took 5 minutes

--- src/main/java/net/evilkingdom/commons/utilities/luckperms/LuckPermsUtilities.java
+++ src/main/java/net/evilkingdom/commons/utilities/luckperms/LuckPermsUtilities.java
@@ -6,16 +6,31 @@ package net.evilkingdom.commons.utilities.luckperms;
 
 import net.luckperms.api.LuckPermsProvider;
 
+import java.security.Permission;
 import java.util.ArrayList;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public class LuckPermsUtilities {
 
+    public static ArrayList<String> getPermissionsViaCache(final UUID uuid) {
+        final ArrayList<String> permissions = new ArrayList<String>();
+        LuckPermsProvider.get().getUserManager().getUser(uuid).getCachedData().getPermissionData().getPermissionMap().forEach((permission, state) -> {
+            if (state) {
+                permissions.add(permission);
+            }
+        });
+        return permissions;
+    }
+
+    public static ArrayList<String> getRanksViaCache(final UUID uuid) {
+        return new ArrayList<String>(getPermissionsViaCache(uuid).stream().filter(permission -> permission.startsWith("group.")).toList());
+    }
+
     public static CompletableFuture<ArrayList<String>> getPermissions(final UUID uuid) {
         return LuckPermsProvider.get().getUserManager().loadUser(uuid).thenApply(user -> {
             final ArrayList<String> permissions = new ArrayList<String>();
-            user.getCachedData().getPermissionData().getPermissionMap().forEach((permission, state) -> {
+            LuckPermsProvider.get().getUserManager().getUser(uuid).getCachedData().getPermissionData().getPermissionMap().forEach((permission, state) -> {
                 if (state) {
                     permissions.add(permission);
                 }
@@ -25,15 +40,7 @@ public class LuckPermsUtilities {
     }
 
     public static CompletableFuture<ArrayList<String>> getRanks(final UUID uuid) {
-        return LuckPermsProvider.get().getUserManager().loadUser(uuid).thenCompose(user -> getPermissions(uuid).thenApply(permissions -> {
-            final ArrayList<String> ranks = new ArrayList<String>();
-            permissions.forEach(permission -> {
-                if (permission.startsWith("group.")) {
-                    ranks.add(permission.replaceFirst("group.", ""));
-                }
-            });
-            return ranks;
-        }));
+        return LuckPermsProvider.get().getUserManager().loadUser(uuid).thenCompose(user -> getPermissions(uuid).thenApply(permissions -> new ArrayList<String>(permissions.stream().filter(permission -> permission.startsWith("group.")).toList())));
     }
 
 }
