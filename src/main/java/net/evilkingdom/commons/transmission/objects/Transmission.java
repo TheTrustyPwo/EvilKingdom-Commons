07f297b4524722faa44578523c92084ddeba2f90
kodirati <me@kodirati.com>
2022-05-08 20:53:32 -0500

    Let's hope this is the final fix.
    Signed-off-by: kodirati <me@kodirati.com>
    
    Took 3 minutes

--- src/main/java/net/evilkingdom/commons/transmission/objects/Transmission.java
+++ src/main/java/net/evilkingdom/commons/transmission/objects/Transmission.java
@@ -47,14 +47,12 @@ public class Transmission {
     public CompletableFuture<String> send() {
         final TransmissionTask task = new TransmissionTask(this.site, this.type, this.targetServerName, this.targetSiteName, this.uuid, this.data);
         task.start();
-        Bukkit.broadcastMessage("task started!");
         if (this.type == TransmissionType.REQUEST) {
             return CompletableFuture.supplyAsync(() -> {
                 while (task.isRunning()) {
                     //Does nothing, just to halt the response until the task is done running.
                     //It will also detect if the response is taking too long and just respond that the request failed.
                 }
-                Bukkit.broadcastMessage("response received");
                 return task.getResponseData();
             });
         } else {
