53594e460d511cc52faef499cd96b63ca1f84077
kodirati <me@kodirati.com>
2022-05-15 18:06:32 -0500

    Added utility for filtering in string utilities.
    Signed-off-by: kodirati <me@kodirati.com>
    
    Took 6 minutes

--- src/main/java/net/evilkingdom/commons/utilities/string/StringUtilities.java
+++ src/main/java/net/evilkingdom/commons/utilities/string/StringUtilities.java
@@ -6,6 +6,7 @@ package net.evilkingdom.commons.utilities.string;
 
 import net.md_5.bungee.api.ChatColor;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,6 +26,26 @@ public class StringUtilities {
             matcher.appendReplacement(buffer, ChatColor.of("#" + matcher.group(1)).toString());
         }
         return ChatColor.translateAlternateColorCodes('&', matcher.appendTail(buffer).toString());
+    }
+
+    /**
+     * Allows you to validate if a string has any of the listed words in it.
+     * It will automatically detect replacements of numbers and other characters.
+     *
+     * @param string ~ The string that needs to be validated.
+     * @return If the string has any of the listed words in it.
+     */
+    public static boolean has(String string, final ArrayList<String> words) {
+        string = string.replace("1","i").replace("!","i").replace("3","e").replace("4","a").replace("@","a").replace("5","s").replace("7","t").replace("0","o").replace("9","g").toLowerCase().replaceAll("[^a-zA-Z]", "");
+        for (int start = 0; start < string.length(); start++) {
+            for (int offset = 1; offset < string.length() + 1 - start; offset++)  {
+                final String wordToValidate = string.substring(start, start + offset);
+                if (words.contains(wordToValidate)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
