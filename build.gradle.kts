5ad34b1a035a10ac751529bfb05814e83654cdb6
kodirati <me@kodirati.com>
2022-05-15 23:18:54 -0500

    MASSIVE gradle changes etc
    Signed-off-by: kodirati <me@kodirati.com>
    
    Took 4 minutes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("com.github.johnrengelman.shadow").version("7.1.2")
+    id("net.minecrell.plugin-yml.bukkit").version("0.5.1")
     id("io.papermc.paperweight.userdev").version("1.3.5")
 }
 
@@ -20,8 +21,8 @@ dependencies {
     compileOnly("net.luckperms:api:5.4")
     implementation("org.mongodb:mongo-java-driver:3.12.10")
     compileOnly("me.lucko:spark-api:0.1-SNAPSHOT")
-    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core:2.1.1")
-    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit:2.1.1") {
+    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core:2.1.2")
+    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit:2.1.2") {
         isTransitive = false
     }
 }
@@ -33,5 +34,33 @@ tasks {
     compileJava {
         options.release.set(17)
         options.encoding = "UTF-8"
+    }
+}
+
+bukkit {
+    name = "Commons"
+    author = "kodirati"
+    website = "kodirati.com"
+    description = "Used as the library for the servers of Evil Kingdom."
+    main = "net.evilkingdom.commons.Commons"
+    version = "commit-" + gitCommit()
+    apiVersion = "1.18"
+    depend = listOf("FastAsyncWorldEdit", "LuckPerms")
+}
+
+fun gitCommit(): String {
+    return try {
+        val byteOut = org.apache.commons.io.output.ByteArrayOutputStream()
+        project.exec {
+            commandLine = "git rev-parse --short HEAD".split(" ")
+            standardOutput = byteOut
+        }
+        String(byteOut.toByteArray()).trim().also {
+            if (it == "HEAD")
+                logger.warn("Unable to determine current commit: Project is checked out with detached head!")
+        }
+    } catch (e: Exception) {
+        logger.warn("Unable to determine current commit: ${e.message}")
+        "Unknown Commit"
     }
 }
